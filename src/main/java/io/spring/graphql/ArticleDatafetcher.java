8f7f4c5d65e34290b3beafdf0b87355c34ab491b
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-12-16 16:43:45 +0900

    Refactor/codebase english (#8)
    * refactor: 중국어 사용 부분 영어로 교체
    
    * refactor: 다국어 테스트 중국어에서 한글로 교체

--- src/main/java/io/spring/graphql/ArticleDatafetcher.java
+++ src/main/java/io/spring/graphql/ArticleDatafetcher.java
@@ -48,7 +48,7 @@ public class ArticleDatafetcher {
       @InputArgument("before") String before,
       DgsDataFetchingEnvironment dfe) {
     if (first == null && last == null) {
-      throw new IllegalArgumentException("first 和 last 必须只存在一个");
+      throw new IllegalArgumentException("'first' and 'last' cannot both be null.");
     }
 
     User current = SecurityUtil.getCurrentUser().orElse(null);
@@ -94,7 +94,7 @@ public class ArticleDatafetcher {
       @InputArgument("before") String before,
       DgsDataFetchingEnvironment dfe) {
     if (first == null && last == null) {
-      throw new IllegalArgumentException("first 和 last 必须只存在一个");
+      throw new IllegalArgumentException("'first' and 'last' cannot both be null.");
     }
 
     Profile profile = dfe.getSource();
@@ -144,7 +144,7 @@ public class ArticleDatafetcher {
       @InputArgument("before") String before,
       DgsDataFetchingEnvironment dfe) {
     if (first == null && last == null) {
-      throw new IllegalArgumentException("first 和 last 必须只存在一个");
+      throw new IllegalArgumentException("'first' and 'last' cannot both be null.");
     }
 
     User current = SecurityUtil.getCurrentUser().orElse(null);
@@ -198,7 +198,7 @@ public class ArticleDatafetcher {
       @InputArgument("before") String before,
       DgsDataFetchingEnvironment dfe) {
     if (first == null && last == null) {
-      throw new IllegalArgumentException("first 和 last 必须只存在一个");
+      throw new IllegalArgumentException("'first' and 'last' cannot both be null.");
     }
 
     User current = SecurityUtil.getCurrentUser().orElse(null);
@@ -254,7 +254,7 @@ public class ArticleDatafetcher {
       @InputArgument("withTag") String withTag,
       DgsDataFetchingEnvironment dfe) {
     if (first == null && last == null) {
-      throw new IllegalArgumentException("first 和 last 必须只存在一个");
+      throw new IllegalArgumentException("'first' and 'last' cannot both be null.");
     }
 
     User current = SecurityUtil.getCurrentUser().orElse(null);
