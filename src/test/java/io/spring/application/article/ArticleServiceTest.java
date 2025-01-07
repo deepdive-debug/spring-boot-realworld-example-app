beee97499c60487ef3c7bc8658e0f5707e313234
LeeShinHaeng <dragon11796@gmail.com>
2025-01-07 14:25:01 +0900

    refactor: ArticleServiceTest Mock 제거

--- src/test/java/io/spring/application/article/ArticleServiceTest.java
+++ src/test/java/io/spring/application/article/ArticleServiceTest.java
@@ -3,13 +3,6 @@ package io.spring.application.article;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.anyList;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import io.spring.api.article.request.NewArticleParam;
 import io.spring.api.article.request.UpdateArticleParam;
@@ -19,147 +12,123 @@ import io.spring.api.common.response.PaginatedListResponse;
 import io.spring.api.exception.NoAuthorizationException;
 import io.spring.api.exception.ResourceNotFoundException;
 import io.spring.core.article.domain.Article;
-import io.spring.core.article.domain.ArticleRepository;
-import io.spring.core.article.domain.TagRepository;
+import io.spring.core.article.infrastructure.FakeArticleRepository;
+import io.spring.core.article.infrastructure.FakeTagRepository;
 import io.spring.core.user.domain.User;
-import java.util.List;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+import java.util.List;
+
 public class ArticleServiceTest {
+	private ArticleService articleService;
 
-  @Mock private ArticleRepository articleRepository;
+	private User user;
+	private Article article;
 
-  @Mock private TagRepository tagRepository;
+	@BeforeEach
+	void setUp() {
+		FakeArticleRepository fakeArticleRepository = new FakeArticleRepository();
+		FakeTagRepository fakeTagRepository = new FakeTagRepository();
+		this.articleService = new ArticleService(fakeArticleRepository, fakeTagRepository);
 
-  @InjectMocks private ArticleService articleService;
+		user = User.of("test@test.com", "testUser", "password", "bio", "image");
+		article = Article.create("Test Title", "Test Description", "Test Body", user);
 
-  private User user;
-  private Article article;
+		fakeArticleRepository.save(article);
+	}
 
-  @BeforeEach
-  void setUp() {
-    MockitoAnnotations.openMocks(this);
+	@Test
+	public void should_create_article_successfully() {
+		// given
+		NewArticleParam param =
+			new NewArticleParam("Title", "Description", "Body", List.of("tag1", "tag2"));
 
-    user = User.of("test@test.com", "testUser", "password", "bio", "image");
-    article = Article.create("Test Title", "Test Description", "Test Body", user);
-  }
+		// when
+		Article createdArticle = articleService.createArticle(param, user);
 
-  //  @Test
-  public void should_create_article_successfully() {
-    // given
-    NewArticleParam param =
-        new NewArticleParam("Title", "Description", "Body", List.of("tag1", "tag2"));
-    when(articleRepository.save(any(Article.class))).thenReturn(article);
+		// then
+		assertNotNull(createdArticle);
+		assertEquals("Title", createdArticle.getTitle());
+	}
 
-    // when
-    Article createdArticle = articleService.createArticle(param, user);
+	@Test
+	public void should_get_article_by_slug_successfully() {
+		// when
+		ArticleResponse response = articleService.getArticle("test-title");
 
-    // then
-    assertNotNull(createdArticle);
-    assertEquals("Test Title", createdArticle.getTitle());
-    verify(tagRepository, times(1)).saveAll(anyList());
-    verify(articleRepository, times(1)).save(any(Article.class));
-  }
+		// then
+		assertNotNull(response);
+		assertEquals(article.getSlug(), response.slug());
+	}
 
-  //  @Test
-  public void should_get_article_by_slug_successfully() {
-    // given
-    when(articleRepository.findBySlug(anyString())).thenReturn(Optional.of(article));
+	@Test
+	public void should_throw_exception_when_article_not_found() {
+		// when & then
+		assertThrows(ResourceNotFoundException.class, () -> articleService.getArticle("invalid-slug"));
+	}
+// TODO User id 생성 방법
+	//	@Test
+	public void should_update_article_successfully() {
+		// given
+		UpdateArticleParam param =
+			new UpdateArticleParam("Updated Title", "Updated Description", "Updated Body");
 
-    // when
-    ArticleResponse response = articleService.getArticle("test-title");
+		// when
+		articleService.updateArticle("test-title", user, param);
 
-    // then
-    assertNotNull(response);
-    assertEquals(article.getSlug(), response.slug());
-  }
+		// then
+		assertEquals("Updated Title", article.getTitle());
+	}
 
-  @Test
-  public void should_throw_exception_when_article_not_found() {
-    // given
-    when(articleRepository.findBySlug(anyString())).thenReturn(Optional.empty());
+	//	@Test
+	public void should_throw_exception_when_user_not_author_of_article() {
+		// given
+		User anotherUser = User.of("another@test.com", "anotherUser", "password", "bio", "image");
+		UpdateArticleParam param =
+			new UpdateArticleParam("Updated Title", "Updated Description", "Updated Body");
 
-    // when & then
-    assertThrows(ResourceNotFoundException.class, () -> articleService.getArticle("invalid-slug"));
-  }
+		// when & then
+		assertThrows(
+			NoAuthorizationException.class,
+			() -> articleService.updateArticle("test-title", anotherUser, param));
+	}
 
-  //  @Test
-  public void should_update_article_successfully() {
-    // given
-    UpdateArticleParam param =
-        new UpdateArticleParam("Updated Title", "Updated Description", "Updated Body");
-    when(articleRepository.findBySlug(anyString())).thenReturn(Optional.of(article));
+//	@Test
+	public void should_delete_article_successfully() {
+		// when
+		articleService.deleteArticle("test-title", user);
 
-    // when
-    articleService.updateArticle("test-title", user, param);
+		// then
+		assertThrows(ResourceNotFoundException.class, () -> articleService.getArticle("test-title"));
+	}
 
-    // then
-    assertEquals("Updated Title", article.getTitle());
-    verify(articleRepository, times(1)).findBySlug(anyString());
-  }
+//	@Test
+	public void should_throw_exception_when_deleting_article_with_unauthorized_user() {
+		// given
+		User anotherUser = User.of("another@test.com", "anotherUser", "password", "bio", "image");
 
-  //  @Test
-  public void should_throw_exception_when_user_not_author_of_article() {
-    // given
-    User anotherUser = User.of("another@test.com", "anotherUser", "password", "bio", "image");
-    UpdateArticleParam param =
-        new UpdateArticleParam("Updated Title", "Updated Description", "Updated Body");
+		// when & then
+		assertThrows(
+			NoAuthorizationException.class,
+			() -> articleService.deleteArticle("test-title", anotherUser));
+	}
 
-    when(articleRepository.findBySlug(anyString())).thenReturn(Optional.of(article));
+	@Test
+	public void should_get_paginated_article_list() {
+		// given
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt").descending());
+		Page<Article> articlePage = new PageImpl<>(List.of(article));
 
-    // when & then
-    assertThrows(
-        NoAuthorizationException.class,
-        () -> articleService.updateArticle("test-title", anotherUser, param));
-  }
+		// when
+		PaginatedListResponse<ArticleSummaryResponse> response = articleService.getArticles(0, 10);
 
-  //  @Test
-  public void should_delete_article_successfully() {
-    // given
-    when(articleRepository.findBySlug(anyString())).thenReturn(Optional.of(article));
-
-    // when
-    articleService.deleteArticle("test-title", user);
-
-    // then
-    verify(articleRepository, times(1)).delete(any(Article.class));
-  }
-
-  //  @Test
-  public void should_throw_exception_when_deleting_article_with_unauthorized_user() {
-    // given
-    User anotherUser = User.of("another@test.com", "anotherUser", "password", "bio", "image");
-    when(articleRepository.findBySlug(anyString())).thenReturn(Optional.of(article));
-
-    // when & then
-    assertThrows(
-        NoAuthorizationException.class,
-        () -> articleService.deleteArticle("test-title", anotherUser));
-    verify(articleRepository, never()).delete(any(Article.class));
-  }
-
-  //  @Test
-  public void should_get_paginated_article_list() {
-    // given
-    PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("createdAt").descending());
-    Page<Article> articlePage = new PageImpl<>(List.of(article));
-    when(articleRepository.findAll(pageRequest)).thenReturn(articlePage);
-
-    // when
-    PaginatedListResponse<ArticleSummaryResponse> response = articleService.getArticles(0, 10);
-
-    // then
-    assertEquals(1, response.contents().size());
-    assertEquals(article.getSlug(), response.contents().get(0).slug());
-    verify(articleRepository, times(1)).findAll(pageRequest);
-  }
+		// then
+		assertEquals(1, response.contents().size());
+		assertEquals(article.getSlug(), response.contents().get(0).slug());
+	}
 }
